5de89195c5b2e261e9fac50270c5831270abd342
cathe <cacateles12@gmail.com>
2022-04-28 19:54:49 -0300

    still fixing local storage

--- src/helpers/tokenLocalStorage.js
+++ src/helpers/tokenLocalStorage.js
@@ -1,13 +1,14 @@
-if (!JSON.parse(localStorage.getItem('user'))) {
-  localStorage.setItem('user', JSON.stringify({ email: 'test@user.com' }));
-}
-
 export const addToken = (email) => {
   localStorage.setItem('mealsToken', '1');
   localStorage.setItem('cocktailsToken', '1');
   localStorage.setItem('user', JSON.stringify({ email }));
 };
 
-export const getUser = () => JSON.parse(localStorage.getItem('user'));
+export const getUser = () => {
+  if (!JSON.parse(localStorage.getItem('user'))) {
+    localStorage.setItem('user', JSON.stringify({ email: 'test@project.com' }));
+  }
+  return JSON.parse(localStorage.getItem('user'));
+};
 
 export const clearStorage = () => localStorage.clear();
