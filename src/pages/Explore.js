f4bf4268054475223a5381a6495bab26ff9e0a20
Jacqueline Silva <jacque.sx@hotmail.com>
2022-05-01 10:38:01 -0300

    page explore OK

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
+  const { push } = useHistory();
+
   return (
     <div>
       <Header title="Explore" show={ false } />
+      <div>
+        <button
+          type="button"
+          data-testid="explore-foods"
+          onClick={ () => push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => push('explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </div>
       <Footer />
     </div>
   );
