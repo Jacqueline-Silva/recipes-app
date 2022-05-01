4293607ce1d7ef7b8fa2743e791c9915d682cc22
Jacqueline Silva <jacque.sx@hotmail.com>
2022-05-01 11:04:40 -0300

    70-73

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreDrinks() {
+  const { push } = useHistory();
+
   return (
     <div>
       <Header title="Explore Drinks" show={ false } />
+      <button
+        type="button"
+        name="byIngredient"
+        data-testid="explore-by-ingredient"
+        onClick={ () => push('/explore/drinks/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        name="bySurprise"
+        data-testid="explore-surprise"
+        onClick={ () => {} }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreFoods() {
+  const { push } = useHistory();
+
   return (
     <div>
       <Header title="Explore Foods" show={ false } />
+      <button
+        type="button"
+        name="byIngredient"
+        data-testid="explore-by-ingredient"
+        onClick={ () => push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        name="byNationality"
+        data-testid="explore-by-nationality"
+        onClick={ () => push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        name="bySurprise"
+        data-testid="explore-surprise"
+        onClick={ () => {} }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
